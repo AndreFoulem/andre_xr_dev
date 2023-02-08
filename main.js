08d2e6376965162bb22b8a976dc014a5610d3a9c
AndreFoulem <timmycasual@icloud.com>
2023-02-07 16:47:28 -0800

    Create portfolio

---
+++ main.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.container')
+const sections = gsap.utils.toArray('.container section')
+const texts = gsap.utils.toArray('.anim')
+const mask = document.querySelector('.mask')
+
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.container',
+    pin: true,
+    scrub: 1,
+    end: '+=3500',
+    // snap: 1 / (sections.length - 1),
+    // markers: false,
+  },
+})
+
+console.log(1 / (sections.length - 1))
+
+// whizz around the sections
+sections.forEach((section) => {
+  // grab the scoped text
+  let text = section.querySelectorAll('.anim')
+
+  // bump out if there's no items to animate
+  if (text.length === 0) return
+
+  // do a little stagger
+  gsap.from(text, {
+    y: -130,
+    opacity: 0,
+    duration: 4,
+    ease: 'elastic',
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: section,
+      containerAnimation: scrollTween,
+      // start: 'left center',
+      start: 'left bottom',
+      // markers: true,
+    },
+  })
+})
